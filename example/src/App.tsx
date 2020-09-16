46df1fe279607a22f00a9cd3ecff783c4157b7d1
Zain Memon <zain@inzain.net>
2020-09-16 15:40:29 -0700

    feat: ability to set lockName and keypadCode

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,22 +1,79 @@
-import React, { useState } from 'react';
-import { StyleSheet, View, TouchableOpacity, Text } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import {
+  StyleSheet,
+  View,
+  TouchableOpacity,
+  Text,
+  TextInput,
+} from 'react-native';
+import AsyncStorage from '@react-native-community/async-storage';
 import LyricAccessModal from 'lyric-access';
 
 export default function App() {
   const [modalVisible, setModalVisible] = useState(false);
+  const [lockName, setLockName] = useState('');
+  const [keypadCode, setKeypadCode] = useState('');
+
+  const saveData = async () => {
+    try {
+      await AsyncStorage.setItem('lockName', lockName);
+      await AsyncStorage.setItem('keypadCode', keypadCode);
+    } catch (e) {
+      console.log('Error setting AsyncStorage');
+    }
+  };
+
+  const readData = async () => {
+    try {
+      const retrievedLockName = await AsyncStorage.getItem('lockName');
+      const retrievedKeypadCode = await AsyncStorage.getItem('keypadCode');
+
+      if (retrievedLockName !== null) {
+        setLockName(retrievedLockName);
+      }
+
+      if (retrievedKeypadCode !== null) {
+        setKeypadCode(retrievedKeypadCode);
+      }
+    } catch (e) {
+      console.log('Error reading AsyncStorage');
+    }
+  };
+
+  useEffect(() => {
+    readData();
+  }, []);
+
   return (
     <View style={styles.container}>
+      <Text>Lock Name</Text>
+      <TextInput
+        style={styles.textInput}
+        placeholder={'Ly000...'}
+        onChangeText={setLockName}
+        value={lockName}
+      ></TextInput>
+      <Text>Keypad Code</Text>
+      <TextInput
+        style={styles.textInput}
+        placeholder={'31337'}
+        onChangeText={setKeypadCode}
+        value={keypadCode}
+      ></TextInput>
       <TouchableOpacity
-        onPress={() => setModalVisible(true)}
+        onPress={() => {
+          setModalVisible(true);
+          saveData();
+        }}
         style={styles.appButtonContainer}
       >
-        <Text style={styles.appButtonText}>Unlock my room</Text>
+        <Text style={styles.appButtonText}>Connect</Text>
       </TouchableOpacity>
       <LyricAccessModal
         modalVisible={modalVisible}
         setModalVisible={setModalVisible}
-        lockName={'Ly000005'}
-        keypadCode={'051153'}
+        lockName={lockName}
+        keypadCode={keypadCode}
       />
     </View>
   );
@@ -40,5 +97,15 @@ const styles = StyleSheet.create({
     color: '#fff',
     fontWeight: 'bold',
     alignSelf: 'center',
+  },
+  textInput: {
+    borderColor: '#CCCCCC',
+    borderWidth: 1,
+    height: 50,
+    fontSize: 25,
+    paddingLeft: 20,
+    paddingRight: 20,
+    marginBottom: 20,
+    width: 250,
   },
 });
